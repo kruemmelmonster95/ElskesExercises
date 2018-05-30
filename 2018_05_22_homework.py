32f057f40efc59fd0331f49ba7c60837d483ff64
Elske <elske.ruge@stud.leuphana.de>
2018-05-30 20:16:49 +0200

    homework #1

---
+++ 2018_05_22_homework.py
@@ -0,0 +1,9 @@
+>>> def location(city):
+...             sentence = "It is hot in"
+...             sentence = sentence + city
+...             print(sentence)
+...
+>>> location("Itzehoe")
+It is hot inItzehoe
+>>> location(" Hamburg")
+It is hot in Hamburg
